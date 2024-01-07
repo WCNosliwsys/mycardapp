0ebb94f0dc2f3b4e81c40b001b98d52c30fc344a
Wilson Callisaya <demarktec@gmail.com>
2024-01-07 03:08:22 -0500

    finalizando mycardapp

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,67 @@ class MainPage extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(color: Color(0xff3f50b6)),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(width: double.infinity,),
+            SizedBox(
+              width: double.infinity,
+            ),
             CircleAvatar(
-                radius: 56,              
-                backgroundImage: NetworkImage('https://mymodernmet.com/wp/wp-content/uploads/2021/01/morphy-me-celebrity-face-mashups-15.jpg'),
-                
-              )
-
+              radius: 56,
+              backgroundImage: AssetImage('assets/images/foto.png'),
+            ),
+            Text("Wilson César Callisaya Choquecota",
+                style: TextStyle(
+                    color: Colors.white, fontFamily: "Lobster", fontSize: 16)),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(color: Color(0xff8497e6)),
+            ),
+            SizedBox(
+              child: Divider(color: Color(0xff8497e6)),
+              width: 120,
+            ),
+            MyCard("+51 952000243", "Teléfono", Icons.phone),
+            MyCard("nosliwsys@gmail.com", "Correo electrónico",
+                Icons.email_outlined),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 60.0, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/images/facebook.png',
+                    fit: BoxFit.cover,
+                    width: 50,
+                  ),
+                  Image.asset(
+                    'assets/images/twitter.png',
+                    fit: BoxFit.cover,
+                    width: 50,
+                  ),
+                  Image.asset(
+                    'assets/images/instagram.png',
+                    fit: BoxFit.cover,
+                    width: 50,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  Card MyCard(title, subtitle, IconData icon) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: ListTile(
+        trailing: Icon(Icons.check_circle_outline_outlined),
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(icon),
       ),
     );
   }
